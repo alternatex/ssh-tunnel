96ac492a378d3848860d3dac6b1b25313775289e
alternatex <gianni.furger@gmail.com>
2012-11-23 00:40:23 +0100

    auto-commit

--- install.sh
+++ install.sh
@@ -4,20 +4,20 @@
 cd ~
 
 # fetch sources
-git clone https://github.com/alternatex/tunnelx.git
+git clone https://github.com/alternatex/tunnelx.git .tunnelx
 
 # update shell configuration
 if [ -f ~/.zshrc ]; then 
 	echo "# tunnelx" >> ~/.zshrc
-	echo "export PATH=~/tunnelx/bin:$PATH" >> ~/.zshrc
+	echo "export PATH=~/.tunnelx/bin:$PATH" >> ~/.zshrc
 fi
 
 if [ -f ~/.bashrc ]; then 
 	echo "# tunnelx" >> ~/.bashrc
-	echo "export PATH=~/tunnelx/bin:$PATH" >> ~/.bashrc
+	echo "export PATH=~/.tunnelx/bin:$PATH" >> ~/.bashrc
 fi
 
 if [ -f ~/.profile ]; then 
 	echo "# tunnelx" >> ~/.profile
-	echo "export PATH=~/tunnelx/bin:$PATH" >> ~/.profile
+	echo "export PATH=~/.tunnelx/bin:$PATH" >> ~/.profile
 fi
