ffdd5dce401525f09707641972eae3f1e0715f52
alternatex <gianni.furger@gmail.com>
2013-01-15 02:02:32 +0100

    adjusted formatting

--- lib/min/tunnelx.sh
+++ lib/min/tunnelx.sh
@@ -1,33 +1,33 @@
 #!/bin/bash
 
 function text {
-	printf "\e[1;34m"
-	echo ""
-	echo $1
-	printf "\e[0m"
+  printf "\e[1;34m"
+  echo ""
+  echo $1
+  printf "\e[0m"
 }
 
 function bazinga_edit() {
 
-	# internals
-	BAZINGA_HOME="`dirname $1`/.." && cd $BAZINGA_HOME
-	BAZINGA_INSTALL="`which bazinga 2>&1`"
-	BAZINGA_INSTALL_DIR="`dirname $BAZINGA_INSTALL 2>&1`/.."
+  # internals
+  BAZINGA_HOME="`dirname $1`/.." && cd $BAZINGA_HOME
+  BAZINGA_INSTALL="`which bazinga 2>&1`"
+  BAZINGA_INSTALL_DIR="`dirname $BAZINGA_INSTALL 2>&1`/.."
 
-	# include core
-	source $BAZINGA_INSTALL_DIR/lib/bazinga.sh
+  # include core
+  source $BAZINGA_INSTALL_DIR/lib/bazinga.sh
 
-	# intialize / gather custom configuration
-	bazinga_init $bazinga_namespace
+  # intialize / gather custom configuration
+  bazinga_init $bazinga_namespace
 
-	# shizzl. don't look. todo » solve overwrite data-loss » could be more sexy, right?
-	bazinga_custom="${bazinga_custom}.tmp"
+  # shizzl. don't look. todo » solve overwrite data-loss » could be more sexy, right?
+  bazinga_custom="${bazinga_custom}.tmp"
 
-	# gather custom configuration
-	bazinga_gather
+  # gather custom configuration
+  bazinga_gather
 
-	# replace configurations - write new configuration to disk (actually replacing conf w/ temporary one)
-	bazinga_flush
+  # replace configurations - write new configuration to disk (actually replacing conf w/ temporary one)
+  bazinga_flush
 }
 
 # say hi
@@ -39,6 +39,8 @@ echo "
 printf "\e[0m"
 
 # include configuration (if any)
+  read -p "Configure? («Y» to edit or any key to skip)"
+        ([ "$REPLY" == "y" ] || [ "$REPLY" == "Y" ])  && bazinga_edit
 source ../.bazinga/configure.sh
 
 # move back to directory where we started at (TODO: properly solve path issues)
@@ -48,20 +50,18 @@ cd $currentpath
 export bazinga_directory=".tunnelxc"
 
 # first execution - clean environment?
-if [ ! -d $bazinga_directory ]; then	
+if [ ! -d $bazinga_directory ]; then  
 
-	# create custom directory for tunnelx connection storage
-	echo "Creating new tunnelx connection in directory `pwd`"
-	mkdir $bazinga_directory
+  # create custom directory for tunnelx connection storage
+  echo "Creating new tunnelx connection in directory `pwd`"
+  mkdir $bazinga_directory
 fi
 
 # edit configuration?
-if [ -f ".tunnelxc/settings.sh" ]; then	
-	echo ""
-	read -p "Configure? («Y» to edit or any key to skip)"
-        ([ "$REPLY" == "y" ] || [ "$REPLY" == "Y" ])  && bazinga_edit
+if [ -f ".tunnelxc/settings.sh" ]; then 
+  echo ""
 else 
-	bazinga_edit	
+  bazinga_edit  
 fi
 
 # include custom configuration to feed tunnelx 
