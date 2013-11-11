255e6dec799aca5b8ffd1d22fb504d2c070ce461
alternatex <gianni.furger@gmail.com>
2013-11-11 19:51:10 +0100

    added basic show command

--- test/test.ssh-tunnel.core.js
+++ test/test.ssh-tunnel.core.js
@@ -6,6 +6,6 @@ var program = require('commander')
   , prompt = require('prompt')
   , colors = require('colors')  
   , should = require('should')
-  , tunnel = require('../').tunnel('');
+  , tunnel = require(__dirname+'/../lib/index').tunnel('');
 
 // TODO: implement...
